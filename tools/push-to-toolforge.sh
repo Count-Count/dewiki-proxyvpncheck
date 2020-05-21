ae5ac409bfa1b8f5849370ec859868cca18e4098
Count Count <countvoncount123456@gmail.com>
2020-05-21 13:28:39 +0200

    deployment fixes

--- tools/push-to-toolforge.sh
+++ tools/push-to-toolforge.sh
@@ -3,8 +3,9 @@ set -euxo pipefail
 
 (cd ../; pipenv lock --requirements > /tmp/requirements.txt)
 
-pscp ../{sentinel.py,vpncheck.py,user-config.py} deploy.sh exec-bot.sh *.yaml countcount@login.tools.wmflabs.org:/data/project/dewikivpncheck/
+pscp /tmp/requirements.txt ../{sentinel.py,vpncheck.py,sseclient.py} deploy.sh exec-bot.sh *.yaml countcount@login.tools.wmflabs.org:/data/project/dewikivpncheck/
+pscp ../user-config.py countcount@login.tools.wmflabs.org:/data/project/dewikivpncheck/user-config.py.orig
 plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikivpncheck/exec-bot.sh"
 plink countcount@login.tools.wmflabs.org "chmod 755 /data/project/dewikivpncheck/deploy.sh"
-plink countcount@login.tools.wmflabs.org "become dewikivpncheck chown tools.dewikivpncheck /data/project/dewikivpncheck/user-config.py"
+plink countcount@login.tools.wmflabs.org "become dewikivpncheck cp /data/project/dewikivpncheck/user-config.py.orig /data/project/dewikivpncheck/user-config.py"
 plink countcount@login.tools.wmflabs.org "become dewikivpncheck /data/project/dewikivpncheck/deploy.sh"
